72103f879334d09ba3e9e8a7a720159601b46cbd
Adesh Jadhav <adejad1234@gmail.com>
2024-10-21 13:53:37 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-slate-900 h-screen'>
+      <div className='bg-orange-600 container h-52 w-52'>
+      </div>
+    </div>
+  )
+}
+
+export default App
